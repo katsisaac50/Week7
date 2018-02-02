dbc2135833c1d2b82c7a1cefdeb8fa262713d67d
katsisaac50 <33658908+katsisaac50@users.noreply.github.com>
2018-02-02 10:14:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
-function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
-
-
+function threeFive(startIndex, stopIndex, threeCallback, fiveCallback, bothCallback) {
     let arr = [];
+
     for (let i = startIndex; i <= stopIndex; i++) {
+
         arr.push(i);
     }
+
     console.log(arr);
 
-    function divisibleBythree(startIndex, stopIndex) {
-        if (arr[i] % 3 === 0) {
-            console.log('divided by 3');
+    for (let n = 0; n < arr.length; n++) {
 
-        }
 
-        function divisibleByfive(startIndex, stopIndex) {
-            if (arr[i] % 5 === 0) {
-                console.log('divided by 5');
-            }
-
-            function divisibleByboth(startIndex, stopIndex) {
-                if (arr[i] % 3 === 0 && arr[i] % 5 === 0) {
-                    console.log('divided by both 3 and 5');
-
-                }
-            }
+        if (arr[n] % 3 === 0 && arr[n] % 5 === 0) {
+            bothCallback(arr[n]);
+        } else if (arr[n] % 3 === 0) {
+            threeCallback(arr[n]);
+        } else if (arr[n] % 5 === 0) {
+            fiveCallback(arr[n]);
         }
     }
 }
 
-threeFive(1, 10, "threeCallback", "fiveCallback");
+
+function sayThree(n) {
+    console.log(n + ' is divided by 3');
+
+}
+
+function sayFive(n) {
+    console.log(n + ' is divided by 5');
+}
+
+function sayBoth(n) {
+    console.log(n + ' is divided by both 3 and 5');
+
+}
+
+
+
+threeFive(1, 100, sayThree, sayFive, sayBoth);
